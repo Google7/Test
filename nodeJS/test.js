b3583e0c368623f1cc73d38b759b0c8ef7f2abd5
Google7 <18207156427@139.com>
2019-02-22 15:51:01 +0800

    qaz

--- nodeJS/test.js
+++ nodeJS/test.js
@@ -3,7 +3,7 @@ function list() {
     var i, j, tmp;
     console.log(arr[0] + "--" + arr[1] + "--" + arr[2] + "--" + arr[3] + "--" +
         arr[4] + "--" + arr[5] + "--" + arr[6] + "--" + arr[7] + "--" + arr[8]);
-    console.log("-------------------------")
+    console.log("-------------------------");
     for (i = 0; i < arr.length - 1; i++) {
         for (j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -14,7 +14,7 @@ function list() {
             console.log(arr[0] + "--" + arr[1] + "--" + arr[2] + "--" + arr[3] + "--" +
                 arr[4] + "--" + arr[5] + "--" + arr[6] + "--" + arr[7] + "--" + arr[8]);
         }
-        console.log("-------------------------")
+        console.log("-------------------------");
     }
 }
 
@@ -23,24 +23,17 @@ function Person(name, age) {
     this.age = age;
     this.show = function () {
         console.log(this.name);
-    }
+    };
 }
 var person = new Person("jian", 23);
 
 var arr = [1, 2, 2, 3, 3, 4, 5];
 
-
-var newArr = arr.reduce(function (prev, cur) {
-    prev.indexOf(cur) === -1 && prev.push(cur)
-    return prev;
-}, []);
-console.log(newArr);
-
 function demo(array){
     var temp = [];
-    for (let i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         if(temp.indexOf(array[i]) == -1){
-            temp.push(array[i])
+            temp.push(array[i]);
         }
     }
     return temp;
@@ -49,6 +42,6 @@ function demo(array){
 console.log(demo(arr));
 
 var aa = [1,3,5,2,7,4,8,6];
-console.log(aa.sort().reverse().join(" ").split(/ /))
+console.log(aa.sort().reverse().join(" ").split(/ /));
 
 
