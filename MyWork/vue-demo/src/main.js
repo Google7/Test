1f548c64154f08de1aeaf5845b524f6305600eb1
jianqichen (A) <jWX581422@china.huawei.com>
2019-03-19 17:50:04 +0800

    Vue2+VueRouter2+Webpack+Axios

---
+++ MyWork/vue-demo/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引用工具文件
+import utils from './utils/index.js'
+
+// 引用API文件
+import api from './api/index.js'
+// 将API方法绑定到全局
+Vue.prototype.$api = api
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
+
+// 将工具方法绑定到全局
+Vue.prototype.$utils = utils
---
+++ MyWork/vue-demo/src/page/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="article_list">
+      <ul>
+        <li v-for="i in list" v-bind:key="i.create_at">
+          <time v-text="$utils.goodTime(i.create_at)"></time>
+          <router-link :to="'/content/' + i.id">{{ i.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from "../components/header.vue";
+import Footer from "../components/footer.vue";
+export default {
+  components: { Header, Footer },
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      var v = this;
+      this.$api.get("topics", null, r => {
+        v.list = r.data;
+      });
+    }
+  }
+};
+</script>
